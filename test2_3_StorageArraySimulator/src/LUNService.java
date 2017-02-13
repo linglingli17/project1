70be060230966f4c0280220ce3e61e79aee5406c
linglingli17 <1742191043@qq.com>
2017-02-13 10:26:36 +0800

    update rest web app

--- test2_3_StorageArraySimulator/src/LUNService.java
+++ test2_3_StorageArraySimulator/src/LUNService.java
@@ -5,13 +5,13 @@ import static spark.Spark.*;
 import org.slf4j.spi.*;
 import org.slf4j.impl.*;
 import org.slf4j.*;
-
+import java.lang.Integer;
 public class LUNService {
     public LUNService(final LUNManager lunManager){
 
         //get the size of a LUN
         get("/luns/:id/size", (request, response) -> {
-            Integer idLUN = request.params(":id");
+            Integer idLUN = new Integer(request.params(":id"));
             if (idLUN != null) {
                 int size = lunManager.getLUNSize(idLUN);
                 return "LUN id: " + idLUN + ", size: " + size;
@@ -23,10 +23,10 @@ public class LUNService {
 
         //get the exported host id for a LUN
         get("/luns/:id/export", (request, response) -> {
-            Integer idLUN = request.params(":id");
+            Integer idLUN = new Integer(request.params(":id"));
             if (idLUN != null) {
                 Integer hostID = lunManager.getExportedHostID(idLUN);
-                return "LUN id: " + idLUN + ", export host: " + hostID;
+                return "LUN id: " + idLUN + ", export to host: " + hostID;
             } else {
                 response.status(404); // 404 Not found
                 return "LUN id" + idLUN + "not found";
@@ -34,7 +34,7 @@ public class LUNService {
         });
 
         post("/luns/operation/creation/:size", (request, response) -> {
-            Integer size = request.params(":size");
+            Integer size = new Integer(request.params(":size"));
             Integer idLUN= null;
             if(size != null){
                 idLUN = lunManager.createLUN(size);
@@ -44,57 +44,67 @@ public class LUNService {
             }
             else{
                 response.status(401);
+                return "LUN node found";
             }
         });
 
         post("/luns/operation/creation/:size/:count", (request, response) -> {
-            Integer size = request.params(":size");
-            Integer count = request.params(":count");
+            Integer size = new Integer(request.params(":size"));
+            Integer count = new Integer(request.params(":count"));
 
             if(size != null && count!= null){
                 if(lunManager.createMultipleLUNs(count,size)){
                     response.status(200);
+                    return "Action succeeded";
                 }
             }else{
                     response.status(401);
                 }
+            return "Action failed";
         });
 
         put("/luns/operation/:id/export/:hostid", (request, response) -> {
-            Integer idLUN = request.params(":id");
-            Integer hostID = request.params(":hostid");
+            Integer idLUN = new Integer(request.params(":id"));
+            Integer hostID = new Integer(request.params(":hostid"));
             if (idLUN != null) {
                 if(lunManager.exportLUN(idLUN,hostID)){
                 response.status(200);
+                return "Action succeeded";
             }
         }else{
             response.status(401);
         }
+            return "Action failed";
         });
 
         put("/luns/operation/:id/unexportion", (request, response) -> {
-            Integer idLUN = request.params(":id");
+            Integer idLUN = new Integer(request.params(":id"));
 
             if (idLUN != null) {
                 if( lunManager.unexportLUN(idLUN)){
                     response.status(200);
+                    return "Action succeeded";
                 }
             }else{
                 response.status(401);
             }
+            return "Action failed";
         });
 
-        put("/luns/operation/:id/removal", (request, response) -> {
-            Integer idLUN = request.params(":id");
+        delete("/luns/operation/:id/removal", (request, response) -> {
+            Integer idLUN = new Integer(request.params(":id"));
 
             if (idLUN != null) {
                 if( lunManager.removeUnexportedLUN(idLUN)){
                     response.status(200);
+                    return "Action succeeded";
                 }
             }else{
                 response.status(401);
             }
+            return "Action failed";
         });
+
     }
 
     public static void main(String[] args) {
